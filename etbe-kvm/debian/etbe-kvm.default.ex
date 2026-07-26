b5400e4e2d42c5fafedc5b7310e9254cbf4708e4
Russell Coker <etbe@aaa.coker.com.au>
2026-07-26 14:21:23 +1000

    etbe-kvm 0.1-1

---
+++ etbe-kvm/debian/etbe-kvm.default.ex
@@ -0,0 +1,10 @@
+# Defaults for etbe-kvm initscript
+# sourced by /etc/init.d/etbe-kvm
+# installed at /etc/default/etbe-kvm by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
