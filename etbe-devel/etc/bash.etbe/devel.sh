9d5dd6b4eaf21feb5fe4a99a0f549e99137132a8
Russell Coker <etbe@aaa.coker.com.au>
2026-07-26 14:59:24 +1000

    etbe-devel 0.2

--- etbe-devel/etc/bash.etbe/devel.sh
+++ etbe-devel/etc/bash.etbe/devel.sh
@@ -1,3 +1,3 @@
 alias build-source="wrap-build dpkg-buildpackage -j$(nproc) --changes-option=-S"
-alias dpkg-buildpackage='wrap-build dpkg-buildpackage'
+alias dpkg-buildpackage='wrap-build dpkg-buildpackage -j$(nproc)'
 alias gbp-build-source="wrap-build gbp buildpackage -j$(nproc) --changes-option=-S --git-ignore-new"
